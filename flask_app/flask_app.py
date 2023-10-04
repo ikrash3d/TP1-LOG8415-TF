55098128f13f307c50c2968204df8592ef53bc7e
ming-xiao-yuan <ming-xiao.yuan@outlook.com>
2023-10-03 22:28:14 -0400

    wip:created the flask app on docker locally

--- flask_app/flask_app.py
+++ flask_app/flask_app.py
@@ -1,22 +1,23 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
 def base_route():
-    return "Base Route!"
+    return '<h1>Instance number {} is responding now!</h1>'.format(os.environ['instanceId'])
+
+@app.route('/hello')
+def hello():
+    return '<h1>hello world</h1>'
 
 
-@app.route("/cluster1")
-def cluster1_route():
-    return "Cluster number 1 is responding now!"
+@app.route('/cluster1')
+@app.route('/cluster2')
+def cluster_route():
+    return '<h1>Instance number {} is responding now!</h1>'.format(os.environ['instanceId'])
 
 
-@app.route("/cluster2")
-def cluster2_route():
-    return "Cluster number 2 is responding now!"
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=80)
